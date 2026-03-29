1fb602ba50a1f7d0e89c1a3911dc4206d44e4a62
Ricardo Chavez <ricardo.chavez.t@gmail.com>
2026-03-29 03:07:58 +0200

    Workaround for jekyll-last-modified-at plugin not working correctly for git push deployments

--- _plugins/last_modified_at_generator.rb
+++ _plugins/last_modified_at_generator.rb
@@ -2,7 +2,7 @@
 
 require 'fileutils'
 require 'pathname'
-require 'jekyll-last-modified-at'
+require 'open3'
 
 module Recents
   # Generate change information for all markdown pages
@@ -10,8 +10,20 @@ module Recents
     def generate(site)
       items = site.collections['notes'].docs
       items.each do |page|
-        timestamp = Jekyll::LastModifiedAt::Determinator.new(site.source, page.path, '%FT%T%:z').to_s
-        page.data['last_modified_at_timestamp'] = timestamp
+        stdout_str, stderr_str, status = Open3.capture3("git",
+          "log",
+          "-n",
+          "1",
+          '--format="%ct"',
+          "--",
+          page.path)
+        if !status.success?
+          puts "Error getting last modified date from Git: #{stderr_str}. Using mtime instead."
+          timestamp = File.mtime(page.path)
+        else
+          timestamp = stdout_str.nil? || stdout_str.empty? ? File.mtime(page.path) : stdout_str[/\d+/].to_i
+        end
+        page.data['last_modified_at_timestamp'] = Time.at(timestamp).strftime('%FT%T%:z')
       end
     end
   end
