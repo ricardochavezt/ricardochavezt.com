7efbce12685e975f8779c0d3c27bf0d5756ce37f
Ricardo Chavez <ricardo.chavez.t@gmail.com>
2026-06-14 14:43:58 +0200

    Improving Tumblr recent posts importer
    Added last timestamp check: if no number of recent posts is given,
    the script will check the last time the import was run and only
    import posts that are more recent than that

--- tumblr_recent_posts_importer.rb
+++ tumblr_recent_posts_importer.rb
@@ -7,6 +7,7 @@ require 'fileutils'
 require 'optparse'
 
 IMPORT_DIR = "_posts"
+LAST_IMPORTED_TIMESTAMP_FILE = ".jekyll-cache/tumblr_last_imported"
 
 def extract_json(contents)
     beginning = contents.index("{")
@@ -45,27 +46,44 @@ def write_post(post, use_markdown)
 end
 
 FileUtils.mkdir_p IMPORT_DIR
-num_posts = 1
+num_posts = 0
 OptionParser.new do |parser|
     parser.on("-n", "--number [NUMBER]", Integer, "Number of posts requested") do |num|
         num_posts = num
     end
 end.parse!
 
-feed = URI.parse("https://ricardochavezt.tumblr.com/api/read/json/?num=#{num_posts}").open
+last_imported_timestamp = Time.local(2020)
+if File.exist?(LAST_IMPORTED_TIMESTAMP_FILE)
+    File.open(LAST_IMPORTED_TIMESTAMP_FILE) do |f|
+        last_imported_timestamp = Time.iso8601(f.read) # TODO catch possible parse error
+    end
+    puts "Last imported on: #{last_imported_timestamp}"
+else
+    puts "Last imported timestamp not found, defaulting to #{last_imported_timestamp}"
+end
+
+if num_posts > 0
+    feed_uri = "https://ricardochavezt.tumblr.com/api/read/json/?num=#{num_posts}"
+else
+    feed_uri = "https://ricardochavezt.tumblr.com/api/read/json/"
+end
+feed = URI.parse(feed_uri).open
 contents = feed.readlines.join("\n")
 blog = extract_json(contents)
 blog["posts"].each do |post|
     post_hash = JekyllImport::Importers::Tumblr.post_to_hash(post, "md")
-    if post["reblogged-from-url"]
-        post_hash[:header]["repost_of"] = {"name" => "#{post['reblogged-from-name']}.tumblr.com", "url" => post["reblogged-from-url"]}
-        post_hash[:header]["category"] = "reposts"
+    if Time.iso8601(post_hash[:header]["date"]) > last_imported_timestamp
+        if post["reblogged-from-url"]
+            post_hash[:header]["repost_of"] = {"name" => "#{post['reblogged-from-name']}.tumblr.com", "url" => post["reblogged-from-url"]}
+            post_hash[:header]["category"] = "reposts"
+        end
+        # TODO post type / category
+        write_post(post_hash, true)
     end
-    # TODO post type / category
-    write_post(post_hash, true)
 end
 
-File.open(".jekyll-cache/tumblr_last_imported", "w") do |f|
+File.open(LAST_IMPORTED_TIMESTAMP_FILE, "w") do |f|
     f.puts Time.now.iso8601
 end
 
